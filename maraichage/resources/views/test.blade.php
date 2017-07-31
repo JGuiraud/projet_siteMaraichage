d83326e618a170b7f204ba6112982a949545cddf
JGuiraud <jerome31guiraud@gmail.com>
2017-08-01 00:46:28 +0200

    inside menus bloc added to receive @include parts

--- maraichage/resources/views/test.blade.php
+++ maraichage/resources/views/test.blade.php
@@ -11,18 +11,25 @@
   <section class="menu" id="suggestions" style="background: rgb(192, 57, 43)">
     <div class="preview1"><h1 id="horizontalTitle">La suggestion du Maraicher</h1><h1 id="verticalTitle">suggestions</h1></div>
     <h1 class="name">La suggestion du Maraicher</h1>
-
+    <div class="flexReceiver">
+      <div class="containerSection"></div>
+    </div>
   </section>
 
   <section class="menu" id="markets" style="background: rgb(105,80,101);">
     <div class="preview2"><h1 id="horizontalTitle">Les marchés</h1><h1 id="verticalTitle">marchés</h1></div>
     <h2 class="name">Les marchés</h2>
-
+    <div class="flexReceiver">
+      <div class="containerSection"></div>
+    </div>
   </section>
 
   <section class="menu" id="farm" style="background: #3F5B67;">
     <div class="preview3"><h1 id="horizontalTitle">L'exploitation</h1><h1 id="verticalTitle">exploitation</h1></div>
     <h2 class="name">L'exploitation</h2>
+    <div class="flexReceiver">
+      <div class="containerSection"></div>
+    </div>
 
 
   </section>
