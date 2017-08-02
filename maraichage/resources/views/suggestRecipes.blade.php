03312fddc45e6e2465e9294c6848968ab69de9a1
JGuiraud <jerome31guiraud@gmail.com>
2017-08-02 17:13:45 +0200

    design research result admin done

--- maraichage/resources/views/suggestRecipes.blade.php
+++ maraichage/resources/views/suggestRecipes.blade.php
@@ -1,118 +1,101 @@
-@extends('layouts.app')
-
-@section('content')
+@extends('layouts.app') @section('content')
 
 <div class="container">
-<div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="retour">
-                <a href='/selectionPanier' class="btn btn-primary">Retour</a>
-                <a href="/admin" class="btn btn-danger"><i class="fa fa-backward" aria-hidden="true"></i>
+    <div>
+        <div class="retour">
+            <a href='/selectionPanier' class="btn btn-primary">Retour</a>
+            <a href="/admin" class="btn btn-danger"><i class="fa fa-backward" aria-hidden="true"></i>
                 Retour Menu</a>
-            </div>
+        </div>
+        <h3>Suggestions</h3>
 
-			<h3>Suggestions</h3>
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                Les meilleures suggestions
-                </div>
-                @if($bestOnes)
-                @foreach($bestOnes as $bestResult)
-                    <div class="panel-body">
-                        <span style="text-align:center;"><a href="/details/recette/{{ $bestResult['id'] }}"><i class="fa fa-info-circle detailsRecipe" aria-hidden="true"></i></a></span>
-                        {{ $bestResult['name'] }}
-                        <br>
-                        <ul>matching:
-                            @foreach($bestResult['matching'] as $matching)
-                                <li>{{ $matching }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                    @endforeach
-                @else
-                    <div class="panel-body">
-                        Pas de suggestions pour cette catégorie.
-                        <br>
-                    </div>
-                @endif
-            </div>
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                Les autres suggestions
-                </div>
-                @if($goodOnes)
-                    @foreach($goodOnes as $goodResult)
-                        <div class="panel-body">
-                        <span style="text-align:center;"><a href="/details/recette/{{ $goodResult['id'] }}"><i class="fa fa-info-circle detailsRecipe" aria-hidden="true"></i></a></span>
-                            {{ $goodResult['name'] }}
-                            <br>
-                            <ul>Matching:
-                                @foreach($goodResult['matching'] as $matching)
-                                    <li>{{ $matching }}</li>
-                                @endforeach
-                            </ul>
-                            @if($goodResult['missingVegiesForRecipe'])
-                            <ul>Manquant:
-                                @foreach($goodResult['missingVegiesForRecipe'] as $missing)
-                                    <li>{{ $missing }}</li>
-                                @endforeach
-                            </ul>
-                            @endif
-                            @if($goodResult['nonMatching'])
-                            <ul>En trop:
-                                @foreach($goodResult['nonMatching'] as $nonMatching)
-                                    <li>{{ $nonMatching }}</li>
-                                @endforeach
-                            </ul>
-                            @endif
-                        </div>
-                    @endforeach
-                @else
-                <div class="panel-body">
-                    Pas de suggestions pour cette catégorie.
+        <div class="panel panel-default">
+            <div class="panel-heading">Les meilleures suggestions</div>
+            <div class="panel-body">
+                @if($bestOnes) @foreach($bestOnes as $bestResult)
+                <div class="resultatCarte col-sm-3">
+
+                    <span style="text-align:center;"><a href="/details/recette/{{ $bestResult['id'] }}"><i class="fa fa-info-circle detailsRecipe" aria-hidden="true"></i></a></span>
+                    <b>{{ $bestResult['name'] }}</b>
                     <br>
-                </div>
-                @endif
-            </div>
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                Peut correspondre
-                </div>
-                @if($junk)
-                    @foreach($junk as $junkResult)
-                        <div class="panel-body">
-                            <span style="text-align:center;"><a href="/details/recette/{{ $junkResult['id'] }}"><i class="fa fa-info-circle detailsRecipe" aria-hidden="true"></i></a></span>
-                            {{ $junkResult['name'] }}
-                            <br>
-                            @if ($junkResult['matching'])
-                            <ul>Matching:
-                                @foreach($junkResult['matching'] as $matching)
-                                    <li>{{ $matching }}</li>
-                                @endforeach
-                            </ul>
-                            @endif
-                            <ul>En trop:
-                                @foreach($junkResult['nonMatching'] as $nonMatching)
-                                    <li>{{ $nonMatching }}</li>
-                                @endforeach
-                            </ul>
-                            <ul>Manquant:
-                                @foreach($junkResult['missingVegiesForRecipe'] as $missing)
-                                    <li>{{ $missing }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
+                    <li>Correspondent : @foreach($bestResult['matching'] as $matching) {{ $matching }} @endforeach
+                    </li>
                     @endforeach
+                </div>
                 @else
-                <div class="panel-body">
+                <div>
                     Pas de suggestions pour cette catégorie.
                     <br>
                 </div>
                 @endif
             </div>
         </div>
+
+
+        <div class="panel panel-default">
+            <div class="panel-heading">Les autres suggestions</div>
+            <div class="panel-body">
+                @if($goodOnes) @foreach($goodOnes as $goodResult)
+                <div class="resultatCarte col-sm-3">
+                    <div class="nomRecetteResultat"><span style="text-align:center;"><a href="/details/recette/{{ $goodResult['id'] }}"><i class="fa fa-info-circle detailsRecipe" aria-hidden="true"></i></a></span>                        <b>{{ $goodResult['name'] }}</b>
+                    </div>
+                    <div class="detailsRecettesResultat">
+                        <li>
+                            <u>Correspondent</u> : @foreach($goodResult['matching'] as $matching) {{ $matching }} @endforeach
+                        </li>
+
+                        @if($goodResult['missingVegiesForRecipe'])
+                        <li>
+                            <u>Manquant</u> : @foreach($goodResult['missingVegiesForRecipe'] as $missing) {{ $missing }} @endforeach
+                        </li>
+                        @endif @if($goodResult['nonMatching'])
+                        <li>
+                            <u>En trop</u> : @foreach($goodResult['nonMatching'] as $nonMatching) {{ $nonMatching }} @endforeach
+                        </li>
+                        @endif
+                    </div>
+                </div>
+                @endforeach @else
+                <div>
+                    Pas de suggestions pour cette catégorie.
+                    <br>
+                </div>
+                @endif
+            </div>
+        </div>
+
+        <div class="panel panel-default">
+            <div class="panel-heading">Peut correspondre</div>
+            <div class="panel-body">
+                @if($junk) @foreach($junk as $junkResult)
+                <div class="resultatCarte col-sm-3">
+                    <div class="nomRecetteResultat"><span style="text-align:center;"><a href="/details/recette/{{ $junkResult['id'] }}"><i class="fa fa-info-circle detailsRecipe" aria-hidden="true"></i></a></span>                        <b>{{ $junkResult['name'] }}</b></div>
+
+                    <div class="detailsRecettesResultat">
+                        <li>
+                            <u>Correspondent</u> : @foreach($junkResult['matching'] as $matching) {{ $matching }} @endforeach
+                        </li>
+
+                        @if($junkResult['missingVegiesForRecipe'])
+                        <li>
+                            <u>Manquant</u> : @foreach($junkResult['missingVegiesForRecipe'] as $missing) {{ $missing }} @endforeach
+                        </li>
+                        @endif @if($junkResult['nonMatching'])
+                        <li>
+                            <u>En trop</u> : @foreach($junkResult['nonMatching'] as $nonMatching) {{ $nonMatching }} @endforeach
+                        </li>
+                        @endif
+                    </div>
+                    @endforeach @else
+                    <div>
+                        Pas de suggestions pour cette catégorie.
+                        <br>
+                    </div>
+                    @endif
+                </div>
+            </div>
+        </div>
     </div>
 </div>
-@endsection
-@section('js')
-@endsection
+
+@endsection @section('js') @endsection
