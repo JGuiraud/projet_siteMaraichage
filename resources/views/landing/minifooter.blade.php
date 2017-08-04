769278a18f7a68406c42d8fe211e6160e188e75e
David <david.diez31@free.fr>
2017-08-04 13:59:08 +0200

    update minifooter

--- resources/views/landing/minifooter.blade.php
+++ resources/views/landing/minifooter.blade.php
@@ -1,4 +1,4 @@
 
-<div class="minifooterLeft">© 2017 -<u><a href="henri.bunot@gmail.com">LesJardinsDeRiton</a></u> - </div>
+<div class="minifooterLeft">© 2017 -<u><a href="mailto:henri.bunot@gmail.com">LesJardinsDeRiton</a></u> - </div>
 <div class=" minifooterRight">Dev : Simplon Occitanie | <a href="/login">Connexion admin</a></div>
 
